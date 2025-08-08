46d7cd5fc728ecec42f010e97a82ada3dff04f2a
Naoko Shoto <mdigreget@gmail.com>
2025-08-08 14:24:56 +0000

    beryl: initial vendor from OS2.0.3.0.VOQTWXM

---
+++ Android.mk
@@ -0,0 +1,21 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),beryl)
+
+$(call add-radio-file-sha1-checked,radio/dpm.img,e38708a372aa3ddb37a4cc76b40c46105fa35a49)
+$(call add-radio-file-sha1-checked,radio/gz.img,78fe5bb1c7aed181f870499beb64f91b3f4f878c)
+$(call add-radio-file-sha1-checked,radio/lk.img,9b15a8904a170aa33967af5ff1f94e273bc4c6d3)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,14d18012c667bacdfae54906c426486cc4f4d27f)
+$(call add-radio-file-sha1-checked,radio/md1img.img,1ce7dad4a3ef8ade5bef9f2eb27364d09a4f5c7b)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,087992731fee269c185a3f8b3bc03a198e3bc443)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,a7e920e3d56aeaa34ca543580d33803628baac5b)
+$(call add-radio-file-sha1-checked,radio/scp.img,b7a70172799fdfa67bfde7cd4d3356c20494cbf3)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,1652f165e4decfdf3afb60e63223065a0eaf73e8)
+$(call add-radio-file-sha1-checked,radio/sspm.img,54b0f86702c054659464a1d8b04740f3816c6628)
+$(call add-radio-file-sha1-checked,radio/tee.img,53b907dc6744874090c835e33ddd0dd535f188ad)
+
+endif
